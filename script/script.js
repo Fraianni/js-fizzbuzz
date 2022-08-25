a5583d8164da4cba0e9ce298affbc64e2fcbac5a
Fraianni <francescoianni1995@gmail.com>
2022-08-25 16:09:28 +0200

    aggiunta colori in base al contenuto

--- script/script.js
+++ script/script.js
@@ -7,17 +7,22 @@ for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log('fizz-buzz');
         new_div.append("fizz-buzz");
+        new_div.style.backgroundColor = "red";
     }
 
     else if (i % 3 === 0) {
         console.log('fizz');
         new_div.append("fizz");
+        new_div.style.backgroundColor = "green";
+
     }
 
 
     else if (i % 5 === 0) {
         console.log('buzz');
         new_div.append("buzz");
+        new_div.style.backgroundColor = "pink";
+
     }
 
 
