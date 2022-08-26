b5b1df73e08d62d73572b618e8a559dde07ab453
Fraianni <francescoianni1995@gmail.com>
2022-08-26 09:36:05 +0200

    miglioramento scrittura condizioni degli if

--- script/script.js
+++ script/script.js
@@ -4,13 +4,13 @@ const grid = document.getElementById('grid');
 for (let i = 1; i <= 100; i++) {
     const new_div = document.createElement('div');
 
-    if (i % 3 === 0 && i % 5 === 0) {
+    if (!(i % 3) && !(i % 5)) {
         console.log('fizz-buzz');
         new_div.append("fizz-buzz");
         new_div.style.backgroundColor = "red";
     }
 
-    else if (i % 3 === 0) {
+    else if (!(i % 3)) {
         console.log('fizz');
         new_div.append("fizz");
         new_div.style.backgroundColor = "green";
@@ -18,7 +18,7 @@ for (let i = 1; i <= 100; i++) {
     }
 
 
-    else if (i % 5 === 0) {
+    else if (!(i % 5)) {
         console.log('buzz');
         new_div.append("buzz");
         new_div.style.backgroundColor = "pink";
